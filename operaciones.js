5c373bfa3d43bad0e096b62ccb38f45595bca2a9
Meghan <afigueroa57@ucol.mx>
2025-05-16 14:02:48 -0600

    Front actualizado, faltan validaciones

--- operaciones.js
+++ operaciones.js
@@ -1,20 +1,20 @@
 export function factorDeParo(factor, xl, xu) {
-    var _a, _b;
+    let _a, _b;
     /* -------------------- Declaración de arrays de objetos -------------------- */
-    var tabulador = [];
-    var resultado = [];
+    let tabulador = [];
+    let resultado = [];
     /* ---------------------- Inicializacion del tabulador ---------------------- */
     addToTabulador(tabulador, xl);
     addToTabulador(tabulador, xu);
     /* ------------------------ Declaracion de variables ------------------------ */
-    var ea; // error relativo
-    var xrNuevo = 0; // para error relativo
-    var xrViejo; // para error relativo
-    var PosiNega; // Par de números positivo y negativo
-    var seraIteracion = /^[0-9]+$/.test(factor); // RegExp busca que en todo el texto solo hayan números, .test(variable) se usa para probar dicho RegExp en la variable regresando un Boolean
-    var numeroDeIteraciones = Number(factor); // Convierte el texto en número
+    let ea; // error relativo
+    let xrNuevo = 0; // para error relativo
+    let xrViejo; // para error relativo
+    let PosiNega; // Par de números positivo y negativo
+    let seraIteracion = /^[0-9]+$/.test(factor); // RegExp busca que en todo el texto solo hayan números, .test(variable) se usa para probar dicho RegExp en la variable regresando un Boolean
+    let numeroDeIteraciones = Number(factor); // Convierte el texto en número
     if (seraIteracion) { // En caso de que sí sea por iteracion
-        for (var i = 1; i <= numeroDeIteraciones; i++) { // i representa la iteracion
+        for (let i = 1; i <= numeroDeIteraciones; i++) { // i representa la iteracion
             PosiNega = obtainPositiveAndNegative(tabulador); // Obtengo los valores desde el tab
             xrViejo = xrNuevo; //Guardo el que tenia
             xrNuevo = xr(PosiNega[0], PosiNega[1]); //Genero uno nuevo
@@ -26,12 +26,12 @@ export function factorDeParo(factor, xl, xu) {
     }
     else {
         /* -------------------------- Si es por porcentaje -------------------------- */
-        var operadorMatch = (_a = factor.match(/^(<=?|>=?|==|!=)/)) === null || _a === void 0 ? void 0 : _a[0];
-        var operador = operadorMatch ? operadorMatch.trim() : '';
+        let operadorMatch = (_a = factor.match(/^(<=?|>=?|==|!=)/)) === null || _a === void 0 ? void 0 : _a[0];
+        let operador = operadorMatch ? operadorMatch.trim() : '';
         // Para ubicar si tiene operador, ?. se usa para acceder al valor solo si éste no es null o 'unidentified' de lo contrario arroja 'unitendified' en lugar de error, ?? se usa en caso de que la sección izquierda al mismo sea 'unidentified', en caso de serlo, arroja el valor de la derecha
-        var valorStr = factor.replace(/^(<=?|>=?|==|!=)\s*/, '');
-        var valor = parseFloat(valorStr); //Para quitar dicho operador
-        var condiciones = {
+        let valorStr = factor.replace(/^(<=?|>=?|==|!=)\s*/, '');
+        let valor = parseFloat(valorStr); //Para quitar dicho operador
+        let condiciones = {
             '<': function (ea, valor) { return ea > valor; },
             '<=': function (ea, valor) { return ea >= valor; },
             '>': function (ea, valor) { return ea < valor; },
@@ -40,8 +40,8 @@ export function factorDeParo(factor, xl, xu) {
             '!=': function (ea, valor) { return ea !== valor; },
             '': function (_, __, iteration) { return iteration <= 100; } // Límite máximo de seguridad
         };
-        var FDP = (_b = condiciones[operador]) !== null && _b !== void 0 ? _b : condiciones['']; // En base al registro obtiene la operación a usar, en caso de ser indefinido arroja la opción por default, que en este caso es ''
-        var iteration = 0; // numero de iteraciones
+        let FDP = (_b = condiciones[operador]) !== null && _b !== void 0 ? _b : condiciones['']; // En base al registro obtiene la operación a usar, en caso de ser indefinido arroja la opción por default, que en este caso es ''
+        let iteration = 0; // numero de iteraciones
         do {
             iteration++; // Obtengo iteración
             if (iteration > 100) { // Límite máximo de iteraciones
@@ -63,11 +63,11 @@ function addToTabulador(tab, x) {
     tab.push({ x: x, fx: (Math.pow(x, 3)) + (Math.pow(x, 2)) - (3 * x) - 3 });
 }
 function obtainPositiveAndNegative(ob) {
-    var res = [];
+    let res = [];
     ob.sort(function (a, b) { return a.x - b.x; }); //ordenar Xs
-    for (var i = 0; i < ob.length - 1; i++) { // Obtener Xs de los FXs necesarios
-        var act = ob[i].fx;
-        var sig = ob[i + 1].fx;
+    for (let i = 0; i < ob.length - 1; i++) { // Obtener Xs de los FXs necesarios
+        let act = ob[i].fx;
+        let sig = ob[i + 1].fx;
         if (act < 0 && sig > 0) {
             res = [ob[i].x, ob[i + 1].x];
             break;
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import {factorDeParo} from './operaciones.js';
+import { factorDeParo } from "./operaciones.js";
 
 // Obtención de los elementos del DOM
 const factorParo = document.getElementById("factorParo");
@@ -7,36 +7,36 @@ let ingresoFactorParo = document.getElementById("ingresoFactorParo");
 let mensajesError = document.getElementById("mensajesError");
 let btnCalcular = document.getElementById("btnCalcular");
 
-factorParo.addEventListener('change', () => {
-    if (factorParo.value === 'noIteraciones') {
-        labelIngresoFactorParo.textContent = 'Número de iteraciones';
-
-        // Mover esto al boton de calcular
-        if (ingresoFactorParo.value.includes('.')) {
-            mensajesError.textContent = "El número de iteraciones no puede contener decimales.";
-            ingresoFactorParo.value= '';
-        }
-    } else if (factorParo.value === 'errorRelativo') {
-        labelIngresoFactorParo.textContent = 'Error relativo';
-    } else {
-        labelIngresoFactorParo.textContent = 'Selección inválida';
-    }
+factorParo.addEventListener("change", () => {
+  if (factorParo.value === "noIteraciones") {
+    labelIngresoFactorParo.textContent = "Número de iteraciones";
+  } else if (factorParo.value === "errorRelativo") {
+    labelIngresoFactorParo.textContent = "Error relativo";
+  } else {
+    labelIngresoFactorParo.textContent = "Selección inválida";
+  }
 });
 
-btnCalcular.addEventListener('click', (e) => {
-    e.preventDefault();
-    // Obtener los elementos del DOM para mostrar los resultados en la tabla
-    const tbodyResults = document.getElementById("tbodyResults");
-    const intervaloInferior = document.getElementById("intervaloA");
-    const intervaloSuperior = document.getElementById("intervaloB");
+btnCalcular.addEventListener("click", (e) => {
+  e.preventDefault();
+  // Obtener los elementos del DOM para mostrar los resultados en la tabla
+  const tbodyResults = document.getElementById("tbodyResults");
+  const intervaloInferior = document.getElementById("intervaloA");
+  const intervaloSuperior = document.getElementById("intervaloB");
+  const valorFactorParo = ingresoFactorParo.value;
 
-    try {
-        // Enviar valores a la función
-    let resultados = factorDeParo(ingresoFactorParo.value, parseFloat(intervaloInferior.value), parseFloat(intervaloSuperior.value));
+  try {
+    if (validarIngresoFactorParo(valorFactorParo)) {
+      // Enviar valores a la función
+      let resultados = factorDeParo(
+        ingresoFactorParo.value,
+        parseFloat(intervaloInferior.value),
+        parseFloat(intervaloSuperior.value)
+      );
 
-    tbodyResults.innerHTML = ''; // Limpiar la tabla antes de agregar nuevos resultados
-    
-    resultados.forEach((aproximacion) => {
+      tbodyResults.innerHTML = ""; // Limpiar la tabla antes de agregar nuevos resultados
+
+      resultados.forEach((aproximacion) => {
         // Creación de los elementos necesarios para mostrar los resultados en la tabla <tr> (fila) y <td> (celda)
         let trResultado = document.createElement("tr");
         let tdIteracion = document.createElement("td");
@@ -49,28 +49,59 @@ btnCalcular.addEventListener('click', (e) => {
         tdEa.textContent = aproximacion.ea; // Valor
         // Validar que no se vea infinity
         if (aproximacion.ea == Infinity) {
-            tdEa.textContent = 'No aplica'; // Si es infinito, se muestra "no aplica"
+          tdEa.textContent = "No aplica"; // Si es infinito, se muestra "no aplica"
         }
 
         // Añadir clases a los elementos para el estilo
-        tdIteracion.classList.add("w-1/6", "border", "border-blue-600", "px-3", "py-2", "text-wrap");
-        tdRaiz.classList.add("w-1/6", "border", "border-blue-600", "px-3", "py-2", "text-wrap");
-        tdEa.classList.add("w-1/6", "border", "border-blue-600", "px-3", "py-2", "text-wrap");
+        tdIteracion.classList.add(
+          "w-1/6",
+          "border",
+          "border-blue-600",
+          "px-3",
+          "py-2",
+          "text-wrap"
+        );
+        tdRaiz.classList.add(
+          "w-1/6",
+          "border",
+          "border-blue-600",
+          "px-3",
+          "py-2",
+          "text-wrap"
+        );
+        tdEa.classList.add(
+          "w-1/6",
+          "border",
+          "border-blue-600",
+          "px-3",
+          "py-2",
+          "text-wrap"
+        );
 
         // Añadir los elementos a la fila y luego la fila al cuerpo de la tabla
         trResultado.appendChild(tdIteracion);
         trResultado.appendChild(tdRaiz);
         trResultado.appendChild(tdEa);
         tbodyResults.appendChild(trResultado);
-    });
-    } catch (error) {
-        console.error("Error al calcular el resultado:", error);
-        mensajesError.textContent = "Error al calcular el resultado. Verifica los valores ingresados.";
-        setTimeout(() => {
-            mensajesError.textContent = '';
-        }, 3000);
+      });
     }
+  } catch (error) {
+    console.error("Error al calcular el resultado:", error);
+    mensajesError.textContent =
+      "Error al calcular el resultado. Verifica los valores ingresados.";
+    setTimeout(() => {
+      mensajesError.textContent = "";
+    }, 3000);
+  }
 }); // Fin de btnCalcular
 
-
 // Validar que intervalo menor sea efectivamente menor que el intervalo mayor
+function validarIngresoFactorParo(valorFactorParo) {
+	if (factorParo.value === "noIteraciones") {
+    	labelIngresoFactorParo.textContent = "Número de iteraciones";
+	} else if (factorParo.value === "errorRelativo") {
+    	labelIngresoFactorParo.textContent = "Error relativo";
+	} else {
+    	labelIngresoFactorParo.textContent = "Selección inválida";
+	}
+}
